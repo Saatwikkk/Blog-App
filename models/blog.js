9bea0cc751b206e505cb46030fdf3737d2a88552
Saatwik Choudhary <choudharysaatwik@gmail.com>
2024-08-11 13:02:49 +0530

    commit

---
+++ models/blog.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    coverImageURL: {
+      type: String,
+      required: false,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+    // profileImageURL: {
+    //   type: String,
+    //   ref: "./public/images/default.png",
+    //   required: true,
+    // }
+  },
+  { timestamps: true }
+);
+
+const Blog = model("blog", blogSchema);
+
+module.exports = Blog;
